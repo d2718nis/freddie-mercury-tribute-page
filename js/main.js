e3bd79dfd9618a01ae07e35692c9d7b273816515
Denis Zharikov <d2718nis@gmail.com>
2017-02-20 01:15:18 +0300

    Fix indents spaces to tabs

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
 
-  // Start Bootstrap carousel
-  $('.carousel').carousel({
-    interval: 3000
-  });
-  
-  // Easter egg
-  $(".title-main").on("click", function() {
-    if ($(".title-main").text() == "FREDDIE MERCURY") {
-      $(".title-main").text("JUST CODE IT.");
-    } else {
-      $(".title-main").text("FREDDIE MERCURY");
-    }
-  });
+	// Start Bootstrap carousel
+	$('.carousel').carousel({
+		interval: 3000
+	});
+	
+	// Easter egg
+	$(".title-main").on("click", function() {
+		if ($(".title-main").text() == "FREDDIE MERCURY") {
+			$(".title-main").text("JUST CODE IT.");
+		} else {
+			$(".title-main").text("FREDDIE MERCURY");
+		}
+	});
 });
