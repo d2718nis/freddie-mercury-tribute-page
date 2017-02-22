9a49664bd76911dc377b23678b4699ee7991f575
Denis Zharikov <d2718nis@gmail.com>
2017-02-22 03:32:15 +0300

    Replace double quotes with single in the main.js
    Made this according to the Google style guides. Also clean up TODO
    section in the index.html file.

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
 	});
 	
 	// Easter egg
-	$(".title-main").on("click", function() {
-		if ($(".title-main").text() == "FREDDIE MERCURY") {
-			$(".title-main").text("JUST CODE IT.");
+	$('.title-main').on('click', function() {
+		if ($('.title-main').text() == 'FREDDIE MERCURY') {
+			$('.title-main').text('JUST CODE IT.');
 		} else {
-			$(".title-main").text("FREDDIE MERCURY");
+			$('.title-main').text('FREDDIE MERCURY');
 		}
 	});
 });
